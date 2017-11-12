09ff876d6acf4ce64742e46c4e8af9fa95683cf1
Alena Martsenyuk <english52009@yandex.ru>
2017-11-12 13:32:21 +0300

    Чтобы бот не ругался

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ s4() + s4();}
 			$('#mainul').append('<li id="' + li_id + '">' +
 					    '<span>' + task_text +
 					    '</span>' +
-					    '<input type="button" value="Удалить" onclick="$(\'#'
+					    '<button onclick="$(\'#'
 					    + li_id + '\').remove()">' +
 					    '</li>')
 			}
@@ -22,7 +22,7 @@ s4() + s4();}
 						 '<hr>' +
 						 '<input id="add_task_input">'
 						 +
-						 '<input type="button" id="add_task" value="Добавить" onclick="add_li($(\'#add_task_input\').val())">');
+						 '<button onclick="add_li($(\'#add_task_input\').val())">');
 					  add_li
 					  ("Сделать задание #3 по web-программированию");}
 			)
